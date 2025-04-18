12b36eb405363bd1201577bbbebd223a81628bd5
FruitfulEjiro <frutfl@outlook.com>
2025-04-18 10:54:31 +0100

    Database Setup

---
+++ server.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+
+// Local Modules
+import connectDB from "./model/DB.js";
+import app from "./app.js";
+
+// set up env file
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
+});
