22383a5571ee03a5cad14035a15f7bd215df8e24
FruitfulEjiro <frutfl@outlook.com>
2025-05-11 12:53:39 +0100

    Project Complete

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ import saveRoutes from "./routes/saveItemRoutes.js";
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(CookieParser());
