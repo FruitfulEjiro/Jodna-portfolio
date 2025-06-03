36a7c7ffcc3ea9d777738d12ff8b383e1297e2f7
FruitfulEjiro <frutfl@outlook.com>
2025-06-03 12:31:48 +0100

    update to cors

--- app.js
+++ app.js
@@ -12,13 +12,7 @@ import saveRoutes from "./routes/saveItemRoutes.js";
 
 const app = express();
 
-app.use(
-   cors({
-      origin: "https://myweb.com",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
-   })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
