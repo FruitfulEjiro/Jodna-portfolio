6b42667d3b8eb0030c6630d3bbbc46e1da421239
FruitfulEjiro <frutfl@outlook.com>
2025-06-08 10:17:26 +0100

    jwt cookie fix

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -9,11 +9,13 @@ const generateToken = (userID) => {
 
 const createSendToken = (user, code, res) => {
    const token = generateToken(user._id);
+   const isProduction = process.env.NODE_ENV === "production";
+
    const cookieOptions = {
       expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      sameSite: "none",
-      secure: true,
+      secure: isProduction, 
+      sameSite: isProduction ? "none" : "lax",
    };
 
    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
